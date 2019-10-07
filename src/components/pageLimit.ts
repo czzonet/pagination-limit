710545bcef01ee998d5a9b2e9d8d920a148b29d2
czz <czzonet@163.com>
2019-10-07 14:13:58 +0800

    test,readme,fix + to - || 增加测试输出  文档 修正算法应该为-

--- src/components/pageLimit.ts
+++ src/components/pageLimit.ts
@@ -3,16 +3,21 @@ const LIMIT = 5
 /**
  * 总数最大页数限制  
  * 算法：列表找规律——总数、最大页数、总数对每页数量，就可以发现
+ * @param count 总数
+ * @returns 最大分页
  */
 const pageLimit = (count: number) => {
   /** 处理负数 设为0 */
   let c = count >= 0 ? count : 0
-  return Math.floor((c + 1) / LIMIT) + 1
+  return Math.floor((c - 1) / LIMIT) + 1
 }
 
 /**
  * 修正当前页  
  * 返回限制最大数值的页面
+ * @param page 当前分页
+ * @param count 总数
+ * @returns 当前分页 超过最大分页时显示为最大分页
  */
 export const limitPage = (page: number, count: number) => {
   let pageLimitNumber = pageLimit(count)
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { limitPage } from "./components/pageLimit";
 
 console.log('Here we are.');
-// limitPage(3,20)
-console.log('limitPage(3,20): ', limitPage(3, 20));
+
+/**
+ * test
+ */
+
+let ps = [1, 2, 3, 4, 5, 6]
+let cs = [0, 12, 20]
+
+for (let i = 0; i < cs.length; i++) {
+  let c = cs[i];
+  for (let j = 0; j < ps.length; j++) {
+    let p = ps[j];
+    console.log(`limitPage(${p},${c}): `, limitPage(p, c))
+  }
+}
+
 debugger
