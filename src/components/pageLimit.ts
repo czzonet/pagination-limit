ccac91b97ed2fe96faae397718bf33a30dc6dd50
czz <czzonet@163.com>
2019-10-07 10:37:28 +0800

    pageLimit init ||限制分页页数算法

---
+++ src/components/pageLimit.ts
@@ -0,0 +1,20 @@
+const LIMIT = 5
+
+/**
+ * 总数最大页数限制  
+ * 算法：列表找规律——总数、最大页数、总数对每页数量，就可以发现
+ */
+const pageLimit = (count: number) => {
+  /** 处理负数 设为0 */
+  let c = count >= 0 ? count : 0
+  return Math.floor((c + 1) / LIMIT) + 1
+}
+
+/**
+ * 修正当前页  
+ * 返回限制最大数值的页面
+ */
+export const limitPage = (page: number, count: number) => {
+  let pageLimitNumber = pageLimit(count)
+  return page <= pageLimitNumber ? page : pageLimitNumber
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+import { limitPage } from "./components/pageLimit";
+
 console.log('Here we are.');
+// limitPage(3,20)
+console.log('limitPage(3,20): ', limitPage(3, 20));
 debugger
