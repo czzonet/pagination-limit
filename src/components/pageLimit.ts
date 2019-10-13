7d692bc7143f6183c7e9c8d4f003ea5da87ea9ef
czz <czzonet@163.com>
2019-10-13 10:47:47 +0800

    limit add || 添加每页数目参数

--- src/components/pageLimit.ts
+++ src/components/pageLimit.ts
@@ -4,10 +4,11 @@ const LIMIT = 5
  * 总数最大页数限制  
  * 算法：列表找规律——总数、最大页数、总数对每页数量，就可以发现
  * @param count 总数
+ * @param limit 每页数目
  * @returns 最大分页
  */
-const pageLimit = (count: number) => {
-  let t = Math.floor((count - 1) / LIMIT) + 1
+const pageLimit = (count: number, limit = LIMIT) => {
+  let t = Math.floor((count - 1) / limit) + 1
   /** 分页最小设为1 */
   return t >= 1 ? t : 1
 }
@@ -17,9 +18,10 @@ const pageLimit = (count: number) => {
  * 返回限制最大数值的页面
  * @param page 当前分页
  * @param count 总数
+ * @param limit 每页数目
  * @returns 当前分页 超过最大分页时显示为最大分页
  */
-export const limitPage = (page: number, count: number) => {
-  let pageLimitNumber = pageLimit(count)
+export const limitPage = (page: number, count: number, limit = LIMIT) => {
+  let pageLimitNumber = pageLimit(count, limit)
   return page <= pageLimitNumber ? page : pageLimitNumber
 }
--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,23 @@ console.log('Here we are.');
 
 let ps = [1, 2, 3, 4, 5, 6]
 let cs = [0, 12, 20]
-
+console.log(`测试：默认每页数量5`);
 for (let i = 0; i < cs.length; i++) {
   let c = cs[i];
+  console.log(`总数为${c}`);
   for (let j = 0; j < ps.length; j++) {
     let p = ps[j];
-    console.log(`limitPage(${p},${c}): `, limitPage(p, c))
+    console.log(`当前分页${p}，修正后： `, limitPage(p, c))
   }
 }
-
+console.log('\n========\n');
+console.log(`测试：每页数量11`);
+for (let i = 0; i < cs.length; i++) {
+  let c = cs[i];
+  console.log(`总数为${c}`);
+  for (let j = 0; j < ps.length; j++) {
+    let p = ps[j];
+    console.log(`当前分页${p}，修正后： `, limitPage(p, c, 11))
+  }
+}
 debugger
